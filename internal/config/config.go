33f2b9aa2ed04f10054aac5998ad3559dbf3ab6f
Igor <k3k13a1ch1k@gmail.com>
2024-04-14 16:34:04 +0300

    add postgres config

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,17 +8,28 @@ import (
 )
 
 type Config struct {
-	Env             string        `yaml:"env"`
-	TokenTTL        time.Duration `yaml:"token_ttl"`
-	RefreshTokenTTL time.Duration `yaml:"refresh_token_ttl"`
-	JWTSecret       string        `yaml:"jwt_secret"`
-	REST            RESTConfig    `yaml:"rest"`
+	Env             string         `yaml:"env"`
+	TokenTTL        time.Duration  `yaml:"token_ttl"`
+	RefreshTokenTTL time.Duration  `yaml:"refresh_token_ttl"`
+	JWTSecret       string         `yaml:"jwt_secret"`
+	REST            RESTConfig     `yaml:"rest"`
+	POSTGRES        PostgresConfig `yaml:"postgres"`
 }
 
 type RESTConfig struct {
 	Host    string        `yaml:"host"`
 	Port    int           `yaml:"port"`
 	Timeout time.Duration `yaml:"timeout"`
+}
+
+type PostgresConfig struct {
+	Host           string `yaml:"host"`
+	Port           int    `yaml:"port"`
+	User           string `yaml:"user"`
+	Password       string `yaml:"password"`
+	Database       string `yaml:"database"`
+	SSLMode        string `yaml:"sslmode"`
+	ConnectTimeout int    `yaml:"connect_timeout"`
 }
 
 func SetupConfig() *Config {
